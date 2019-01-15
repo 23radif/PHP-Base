304b15611bb86ffd892ec19d81dab99c9c8dcc0a
23radif <23radif@gmail.com>
2019-01-15 15:58:23 +0300

    ДЗ будет добавлено в ближайшее время, долго качал и устанавливал Open Server

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalabe=no, intial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+<?php
+echo '<h1>Hello World!</h1>';
+?>
+
+<p><?php echo 'Hello World!'; ?></p>
+</body>
+</html>
